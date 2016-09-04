5937783f9519371c9740e7630c0ee195b271a90d
Bonissauro <bonissauro@gmail.com>
2016-09-04 01:30:29 -0300

    Implementacão do login pelo Facebook, substituindo o do Firebase, que estava implementado.

--- app/src/main/java/br/com/bonysoft/redesocial_iesb/LoginActivity.java
+++ app/src/main/java/br/com/bonysoft/redesocial_iesb/LoginActivity.java
@@ -1,229 +1,113 @@
 package br.com.bonysoft.redesocial_iesb;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-
-import br.com.bonysoft.redesocial_iesb.modelo.Usuario;
-import br.com.bonysoft.redesocial_iesb.servicos.UsuarioService;
-
-import static android.Manifest.permission.READ_CONTACTS;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+
         setContentView(R.layout.activity_login);
 
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        LoginButton loginButton = (LoginButton) this.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+                        Intent it = new Intent(LoginActivity.this, PrincipalActivity.class);
+
+                        startActivity(it);
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(LoginActivity.this, "Operação cancelada.", Toast.LENGTH_LONG).show();
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+
+                        Toast.makeText(LoginActivity.this, exception.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                });
+
+
+        ProfileTracker profileTracker = new ProfileTracker() {
             @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    tentaLogar();
-                    return true;
+            protected void onCurrentProfileChanged(
+                    Profile oldProfile,
+                    Profile currentProfile) {
+
+                if (currentProfile!=null){
+
+                    Toast.makeText(LoginActivity.this, "Olá, " + currentProfile.getName(), Toast.LENGTH_LONG).show();
+
+                    // :TODO - GRAVAR O USUARIO OBTIDO DO SDK DO FACEBOOK
+                    // PANA, GRAVA AQUI NO REALM OS DADOS DA CRIATURA RECEM LOGADA
+                    // TO OLHANDO COMO SE OBTEM O TELEFONE DO CARA NO FB
+
+                }else{
+
+                    if (oldProfile!=null) {
+
+                        Toast.makeText(LoginActivity.this, oldProfile.getName()+" desconectou", Toast.LENGTH_LONG).show();
+
+                    }
                 }
-                return false;
+
+
             }
-        });
-
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tentaLogar();
-            }
-        });
-
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
-
-        mEmailView.setText("bonissauro@gmail.com");
-        mPasswordView.setText("12345678");
-
-
-        mAuth = FirebaseAuth.getInstance();
+        };
 
     }
 
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        //
+        // Caso o cara retorne da ActivityPrincipal para cá "desloga" ele do Facebook e obriga
+        // ele a relogar pra desfazer a caca que ele fez
+        //
+
+        LoginManager.getInstance().logOut();
+
     }
 
-    private void tentaLogar() {
-
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
-        final String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        boolean cancel = false;
-
-        View focusView = null;
-
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-
-        if (cancel) {
-
-            focusView.requestFocus();
-
-        } else {
-
-            showProgress(true);
-
-            mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-
-                            showProgress(false);
-
-                            Log.d("LOGIN", "signInWithEmail:onComplete:" + task.isSuccessful());
-
-                            if (!task.isSuccessful()) {
-
-                                Log.w("LOGIN", "Erro de login");
-
-                                Toast.makeText(LoginActivity.this, "Autenticação inválida.", Toast.LENGTH_LONG).show();
-
-                            }else {
-
-
-                                Usuario u = new Usuario("BONI","bonissauro@gmail.com","1223334444");
-
-                                UsuarioService.gravar(u);
-
-                                Log.w("LOGIN", "Logou com sucesso!");
-                                Toast.makeText(LoginActivity.this, "Faz de conta que logou", Toast.LENGTH_LONG).show();
-
-                                Intent it = new Intent(LoginActivity.this, PrincipalActivity.class);
-
-                                startActivity(it);
-
-                            }
-
-                        }
-                    });
-
-        }
-    }
-
-    private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
+        @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 }
